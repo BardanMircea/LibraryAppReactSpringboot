01e9fa6ab4e186be20146b1f957056945a17c4ee
mircea bardan <bardan.mircea.calin@gmail.com>
2023-03-01 09:47:11 +0100

    updated Navbar and Footer

--- 03-frontend/react-library/src/layouts/NavbarAndFooter/Footer.tsx
+++ 03-frontend/react-library/src/layouts/NavbarAndFooter/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <div className="main-color">
       <footer className="container d-flex flex-wrap justify-content-between align-items-center py-5 main-color">
-        <p className="col-md-4 mb-0 text-white">© Example Library App, Inc</p>
+        <p className="col-md-4 mb-0 text-white">© My Library App</p>
         <ul className="nav navbar-dark col-md-4 justify-content-end">
           <li className="nav-item">
             <Link className="nav-link px-2 text-white" to="/home">
